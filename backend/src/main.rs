4375f855e9d22fda1862cfb69e5a6ad9b9c11302
Sam <weathercontrol@tuta.io>
2025-01-19 19:13:32 -0800

    Initial commit

---
+++ backend/src/main.rs
@@ -0,0 +1,13 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
